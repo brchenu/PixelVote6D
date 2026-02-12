687291e7d9ab1e746be3e1dd9fea788286364d01
Brann <branndev@pm.me>
2026-02-12 20:09:34 +0100

    Update test.py to use unnormlaize func

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent
 
 pvnet = PVNet()
 pvnet.load_state_dict(
-    torch.load("pvnet_checkpoint.pth", weights_only=True)["model_state_dict"]
+    torch.load("checkpoints/pvnet_checkpoint.pth", weights_only=True)["model_state_dict"]
 )
 
 dataset_path = os.path.join(BASE_DIR, "dataset", "ycbv")
@@ -35,33 +35,34 @@ for image, mask, vfield, keypoints_2d in datasetloader:
     image = image.to(device)
     pred_mask, pred_vfield = pvnet(image)
 
-    show_vector_field(
-        img=image[0].cpu().numpy().transpose(1, 2, 0),
-        mask=pred_mask[0].squeeze(0).detach().cpu().numpy(),
-        vector_field=pred_vfield[0].detach().cpu().numpy(),
-        keypoints=keypoints_2d[0].numpy(),
-        step=20,
-        scale_mode="full",
-        mask_img=True,
-        resize_factor=2,
-    )
+    # show_vector_field(
+    #     img=image[0].cpu().numpy().transpose(1, 2, 0),
+    #     mask=pred_mask[0].squeeze(0).detach().cpu().numpy(),
+    #     vector_field=pred_vfield[0].detach().cpu().numpy(),
+    #     keypoints=keypoints_2d[0].numpy(),
+    #     step=20,
+    #     scale_mode="full",
+    #     mask_img=True,
+    #     resize_factor=2,
+    # )
 
-    # img = image[0].cpu().numpy().transpose(1, 2, 0)
-    # img = (img * 255).clip(0, 255).astype(np.uint8)
-    # img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    print(f"image device: {image.device}")
+    img = PVNetTransform.unnormalize_image(image.squeeze())
+    img = img.permute(1, 2, 0).cpu().numpy()
+    img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
-    # prob_map = torch.sigmoid(pred_mask[0])
-    # binary_mask = (prob_map > 0.5).float()
+    prob_map = torch.sigmoid(pred_mask[0])
+    binary_mask = (prob_map > 0.5).float()
 
-    # mask_np = binary_mask.squeeze().cpu().numpy().astype(np.uint8)  # 0/1
-    # mask_3c = np.repeat(mask_np[:, :, None], 3, axis=2)
+    mask_np = binary_mask.squeeze().cpu().numpy().astype(np.uint8)  # 0/1
+    mask_3c = np.repeat(mask_np[:, :, None], 3, axis=2)
 
-    # masked_img = img_bgr * mask_3c  # zero out background
+    masked_img = img_bgr * mask_3c  # zero out background
 
-    # # after masked_img is created
-    # orig_w, orig_h = 640, 480
-    # resized = cv2.resize(masked_img, (orig_w, orig_h), interpolation=cv2.INTER_NEAREST)
-    # cv2.imshow("Masked Image", resized)
-    # cv2.waitKey(0)
+    # after masked_img is created
+    orig_w, orig_h = 640, 480
+    resized = cv2.resize(masked_img, (orig_w, orig_h), interpolation=cv2.INTER_NEAREST)
+    cv2.imshow("Masked Image", resized)
+    cv2.waitKey(0)
 
 cv2.destroyAllWindows()
