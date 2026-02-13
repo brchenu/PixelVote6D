6804a8d53bf2c0097b6def415f132c0572a95b99
Brann <branndev@pm.me>
2026-02-13 19:55:14 +0100

    Multiples changes on the pipeline, still draft version

--- pipeline.py
+++ pipeline.py
@@ -7,14 +7,6 @@ from pathlib import Path
 from bop_toolkit.bop_dataset import BOPDirectDataset
 from bop_toolkit.data_transfrom import PVNetTransform
 
-
-def intersect_line(p1, v1, p2, v2):
-    A = np.array([v1, -v2]).T
-    b = p2 - p1
-    t = np.linalg.solve(A, b)
-    return p1 + t[0] * v1
-
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Device: {device}")
 
@@ -45,85 +37,59 @@ image, mask, vfield, keypoints_2d = dataset[0]
 
 
 class PVNetRansac:
-    def __init__(self, data: torch.Tensor, iter: int):
-        self.reshaped_data = data.reshape(
-            data.size(0) // 2, 2, data.size(1), data.size(2)
-        )
-        self.iter = iter
+    def __init__(self, mask: torch.Tensor, vfield: torch.Tensor, num_iter: int):
+        # vfield shape is (B*2, H, W)
+        self.num_keypoints = vfield.size(0) // 2
+        self.batch_size = self.num_keypoints
 
-    @staticmethod
-    def single_hypothesis(data: torch.Tensor):
-        assert data.dim() == 3 and data.size(0) == 2  # 2, H, W
+        x_components = vfield[: self.num_keypoints]
+        y_components = vfield[self.num_keypoints :]
+        self.vfield = torch.stack([x_components, y_components], dim=1)  # B, 2, H, W
 
-        h, w = data.size()[1:]
-        idx = torch.randperm(h * w)[:2]
+        self.num_iter = num_iter
+        self.valid_index = mask.nonzero()  # N, 2
 
-        def coords(idx: int) -> tuple[int, int]:
-            row = idx // w
-            col = idx % w
-            return (col, row)
-
-        p1 = coords(idx[0])
-        p2 = coords(idx[1])
-
-        vec1_vals = data[:, p1[1], p1[0]]
-        vec2_vals = data[:, p2[1], p2[0]]
-
-        assert vec1_vals.dim() == 1 and vec1_vals.size(0) == 2
-
-        return intersect_line(p1, vec1_vals, p2, vec2_vals)
-
-    @staticmethod
-    def batched_hypothesis(data: torch.Tensor):
+    def batched_hypothesis(self):
         """Batch hypothesis over B keypoits"""
 
-        assert data.dim() == 4 and data.size(1) == 2  # B, 2, H, W
+        assert self.vfield.dim() == 4 and self.vfield.size(1) == 2  # B, 2, H, W
 
-        h, w = data.size()[2:]
-        idx = torch.randperm(h * w)[:2]
+        idx = torch.randperm(self.valid_index.size(0))[:2]  # 2
+        p1 = self.valid_index[idx[0]]
+        p2 = self.valid_index[idx[1]]
 
-        assert idx.dim() == 1 and idx.size(0) == 2
+        v1 = self.vfield[:, :, p1[0], p1[1]]
+        v2 = self.vfield[:, :, p2[0], p2[1]]
 
-        def coords(idx: int) -> tuple[int, int]:
-            row = idx // w
-            col = idx % w
-            return (col, row)
+        assert v1.dim() == 2
+        assert v1.size(0) == self.batch_size
+        assert v1.size(1) == 2
 
-        p1 = torch.Tensor(coords(idx[0]))
-        p2 = torch.Tensor(coords(idx[1]))
+        # convert row, col to x, y
+        p1 = p1.flip(0).float()
+        p2 = p2.flip(0).float()
 
         assert p1.dim() == 1 and p1.size(0) == 2
 
-        vec1_vals = data[:, :, p1[1], p1[0]]
-        vec2_vals = data[:, :, p2[1], p2[0]]
-
-        assert (
-            vec1_vals.dim() == 2
-            and vec1_vals.size(0) == data.size(0)
-            and vec1_vals.size(1) == 2
-        )
-
         # Vectorized intersect line
 
-        A = torch.stack([vec1_vals, -vec2_vals], dim=0).T
+        A = torch.stack([v1, -v2], dim=-1)  # B, 2, 2
+        b = (p2 - p1).unsqueeze(0).expand(self.batch_size, 1).float()  # B, 2
 
-        p1 = p1[None, :].repeat(data.size(0), 0)
-        p2 = p2[None, :].repeat(data.size(0), 0)
-        b = p2 - p1
-
-        assert b.dim == 2 and b.size(0) == data.size(0) and b.size(1) == 2
+        assert A.dim() == 3 and A.size(1) == 2 and A.size(2) == 2
+        assert b.dim() == 2 and b.size(1) == 2
 
         t = torch.linalg.solve(A, b)
 
-        assert t.size(0) == data.size(0)
+        assert t.size(0) == self.batch_size
 
-        return p1 + t[:, 0] * vec1_vals
+        return p1 + t[:, 0] * v1
 
     def score(self, hypothesis: torch.Tensor):
         assert hypothesis.dim() == 2
         assert hypothesis.size(1) == 2  # B, 2
 
-        h, w = self.data.size()[2:]
+        h, w = self.vfield.size()[2:]
         x, y = torch.meshgrid(torch.arange(w), torch.arange(h), indexing="xy")
         coords = torch.stack([x, y], dim=0).float()  # 2, H, W
 
@@ -133,14 +99,14 @@ class PVNetRansac:
         hypo_vec = hypothesis - coords  # B, 2, H, W
         hypo_vec = hypo_vec / torch.norm(hypo_vec, dim=1, keepdim=True)  # B, 2, H, W
 
-        hypo_vec = hypo_vec.reshape(hypo_vec.size(0), 2, -1)
-        data_vec = self.reshaped_data.reshape(self.reshaped_data.size(0), 2, -1)
+        # hypo_vec = hypo_vec.reshape(hypo_vec.size(0), 2, -1)
+        data_vec = self.vfield.reshape(self.batch_size, 2, -1)
 
         assert hypo_vec.dim() == 3 and hypo_vec.size(1) == 2
         assert data_vec.dim() == 3 and data_vec.size(1) == 2
 
-        # dot_product = torch.sum(hypo_vec * data_vec, dim=1)  # B, H*W
-        scores = hypo_vec @ data_vec  # B, H*W
+        scores = torch.sum(hypo_vec * data_vec, dim=1)  # B, H*W
+        # scores = hypo_vec @ data_vec  # B, H*W
 
         THRESHOLD = 0.9
         inliers = scores > THRESHOLD
@@ -151,18 +117,17 @@ class PVNetRansac:
 
     def ransac(self):
 
-        hypo_points = self.batched_hypothesis(self.reshaped_data)
-
-        iter_hypo_points = []
-        iter_consensus = []
-        for _ in range(self.iter):
+        all_hyopt = []
+        all_consensus = []
+        for _ in range(self.num_iter):
+            hypo_points = self.batched_hypothesis()
             consensus, hypo_points = self.score(hypo_points)
-            iter_hypo_points.append(hypo_points)
-            iter_consensus.append(consensus)
+            all_hyopt.append(hypo_points)
+            all_consensus.append(consensus)
 
-        iter_hypo_points = torch.stack(iter_hypo_points, dim=0)  # I, B, 2
-        iter_consensus = torch.stack(iter_consensus, dim=0)  # I, B
+        all_hyopt = torch.stack(all_hyopt, dim=0)  # I, B, 2
+        all_consensus = torch.stack(all_consensus, dim=0)  # I, B
 
         # get max of each batch for iter
-        best_consensus, best_idx = iter_consensus.max(dim=0)  # B
-        best_hypo_points = iter_hypo_points[best_idx, :, :]  # B, 2
+        best_consensus, best_idx = all_consensus.max(dim=0)  # B
+        best_hypo_points = all_hyopt[best_idx, :, :]  # B, 2
